cae71384933031adf52d0ee83d322d3be7ca55a7
壹刀流 <idevebi@163.com>
2025-10-26 16:57:41 +0800

    refactor: 统一将temperature改为temp，更简洁易记
    修改内容：
    - 文档练习1中的temperature改为temp
    - 源代码文件同步修改
    - 所有实例统一使用temp变量名
    
    优势：
    - 变量名简洁统一
    - 减少拼写错误
    - 提高代码可读性

--- src/stage1/05-if-branch/03-exercise-temperature.cpp
+++ src/stage1/05-if-branch/03-exercise-temperature.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-    int temperature = 25;
+    int temp = 25;
 
-    if (temperature >= 30) {
+    if (temp >= 30) {
         std::cout << "炎热，注意防暑" << std::endl;
-    } else if (temperature >= 20) {
+    } else if (temp >= 20) {
         std::cout << "温暖，适合出行" << std::endl;
-    } else if (temperature >= 10) {
+    } else if (temp >= 10) {
         std::cout << "凉爽，注意保暖" << std::endl;
     } else {
         std::cout << "寒冷，注意防寒" << std::endl;
